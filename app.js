897baeba4daf2af6858c061f92ee8242e21bf191
海斗 <104343866+hosoyakaito@users.noreply.github.com>
2022-10-28 05:52:12 +0000

    改行直し

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
-
 //aaaaa
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
